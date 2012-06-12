e2ac6a95747713e4d946b01d66f1caa5eb185eeb
Ben Stott <bgbnbigben@contextualsystems.com>
2012-06-12 20:50:13 +1000

    Skeleton code of what I *think* will work

--- listee.h
+++ listee.h
@@ -47,5 +47,29 @@ namespace listee {
        variables in a lambda function, and calling this function as a boolean
        predicate
     */
+    #define new_unary_operator(x, precedence) LISTEE_OP_##x precedence
+
+    #define new_binary_operator(x, precedence) precedence LISTEE_OP_##x precedence
+
+
+    /*  It feels like a parse order makes sense as the following:
+     *  take (t ((with (t_0 in iter_0)) ... (with (t_i in iter_i))) where (p)).
+     *  This implies that 'in' has to have the highest precedence order. Next
+     *  will be 'with', followed by 'where', followed by 'take'.
+     */
+    #define take new_unary_operator(take, )
+
+    /*  Binary operator; operates on the element before it (the 't') and the
+     *  element in front of it. If the element in front is of the same type as
+     *  the with clause this will have to be specialised to make life make
+     *  sense again
+    */
+    #define with new_binary_operator(with, )
+
+    /*  Binary operator; operates on the variable and the iterable */
+    #define in new_binary_operator(in, )
+
+    /*  Binary operator; operates on the predicate and the with clauses */
+    #define where new_binary_operator(where, )
 };
 #endif // header guard
